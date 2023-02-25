59bc211d04fa13ce0be94d68e5facad3fc0406d1
Ezequiel Arenilla <ezequielarenilla@gmail.com>
2023-02-25 23:56:38 +0100

    sa

--- bot.py
+++ bot.py
@@ -3,10 +3,10 @@ import os
 import random
 
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters, PicklePersistence
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
-VIDEO_API = 'https://framex-dev.wadrid.net/api/video/Falcon%20Heavy%20Test%20Flight%20(Hosted%20Webcast)-wbSwFU6tY1c/frame/'
+VIDEO_API = 'https://framex-dev.wadrid.net/api/video/Falcon%20Heavy%20Test%20Flight%20(Hosted%20Webcast)-wbSwFU6tY1c/frame'
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
@@ -18,12 +18,12 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=f'{VIDEO_API}{random.randint(0, 61695)}/')
-    await context.bot.send_photo(chat_id=update.effective_chat.id, photo=f'{VIDEO_API}{random.randint(0, 61695)}/')
-    # await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+    await context.bot.send_photo(chat_id=update.effective_chat.id, photo=f'{VIDEO_API}/{random.randint(0, 61695)}/')
 
-if __name__ == '__main__':
-    application = ApplicationBuilder().token(BOT_TOKEN).build()
+
+def main():
+    persistence = PicklePersistence(filepath='persistent_data')
+    application = ApplicationBuilder().token(BOT_TOKEN).persistence(persistence=persistence).build()
 
     start_handler = CommandHandler('start', start)
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
@@ -32,4 +32,8 @@ if __name__ == '__main__':
     application.add_handler(echo_handler)
 
     application.run_polling()
+
+
+if __name__ == '__main__':
+    main()
 
