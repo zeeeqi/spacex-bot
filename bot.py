143871a6ae862cbb27ae3eb788a6db45ca22da2f
Ezequiel Arenilla <ezequielarenilla@gmail.com>
2023-02-25 23:13:55 +0100

    [MOD] Bot token to env

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 import logging
+import os
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
 
-TOKEN = '6207733786:AAF2cSSIJTgxldRT7IwkH2Tm1clSKLGU9jk'
+BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -14,11 +15,17 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
 if __name__ == '__main__':
-    application = ApplicationBuilder().token(TOKEN).build()
+    application = ApplicationBuilder().token(BOT_TOKEN).build()
 
     start_handler = CommandHandler('start', start)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+
     application.add_handler(start_handler)
+    application.add_handler(echo_handler)
 
     application.run_polling()
 
