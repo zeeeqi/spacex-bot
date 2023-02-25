220e4d1796629459df1e0a920c224eac5d3351e0
Ezequiel Arenilla <ezequielarenilla@gmail.com>
2023-02-25 23:19:13 +0100

    Testing sending random frame on user input

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
 import logging
 import os
+import random
+
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
-
+VIDEO_API = 'https://framex-dev.wadrid.net/api/video/Falcon%20Heavy%20Test%20Flight%20(Hosted%20Webcast)-wbSwFU6tY1c/frame/'
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
@@ -16,7 +18,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+    await context.bot.send_photo(chat_id=update.effective_chat.id, photo=f'{VIDEO_API}/{random.randint(0, 61695)}')
+    # await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(BOT_TOKEN).build()
